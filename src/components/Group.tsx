9935a58e88e495b1d569828371df54ae6505bbea
Michael Fiedler <michael.fiedler1604@gmail.com>
2026-08-30 07:36:44 +0200

    Made group memebers draggable

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,12 +1,19 @@
-import type { User } from "../App";
+import type { User } from "../App"
+import { useRef } from "react"
+import Draggable from 'react-draggable'
 
 function Group({ children, index }: { children: User[]; index: number }) {
   // console.log(children)
+  const nodeRef = useRef(null)
   return (
     <div className='group-container'>
       <h2 style={{color: "var(--primary-accent-color)"}}>Gruppe {index+1}</h2>
         <ul>
-          {children.map((member: User) => <li key={member.id}>{member.vorname} {member.nachname}</li>)}
+          {children.map((member: User) =>
+            <Draggable key={member.id} nodeRef={nodeRef}>
+              <li ref={nodeRef}>{member.vorname} {member.nachname}</li>
+            </Draggable>
+          )}
         </ul>
     </div>
   )
